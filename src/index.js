c846493b022bdec47a047a29204dd02f1bbc54e5
Yoana Yavasheva <yyavasheva@gmail.com>
2024-06-07 12:51:45 +0100

    Added current location feature

--- src/index.js
+++ src/index.js
@@ -28,6 +28,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityTime = moment().tz(cityTimeZone);
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let citiesElement = document.querySelector("#cities");
